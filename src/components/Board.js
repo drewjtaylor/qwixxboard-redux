a872845902d9c7adf5001915fe25b1f15cb9794d
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-07-14 14:10:59 -0400

    Refactoring all around

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,18 +2,17 @@ import { Container, Col, Row } from "react-bootstrap";
 import NumberBox from "./NumberBox";
 import TotalsBox from "./TotalsBox";
 import PenaltyBox from "./PenaltyBox";
+import LockBox from "./LockBox";
 
 import { useSelector, useDispatch } from "react-redux";
 import { toggleBoxValue, resetBoard } from "../utils/scoreSlice";
 
 const Board = () => {
-    const state = useSelector((state) => state);
-    const {score} = state;
+    const score = useSelector((state) => state.score);
     const {row1, row2, row3, row4, penalties} = score;
     const dispatch = useDispatch();
 
-
-    function scoreRow(rowArray) { //function takes the array for a given row
+    const scoreRow = (rowArray) => { //function takes the array for a given row (such as "row1")
         let score = 0;
         let numberofmarks = rowArray.reduce((prev, current) => prev + current);  //reducer supplies us with the total of "marks" (boxes that have a value of 1)
         while (numberofmarks > 0) {  //for however many have a "1", we add the total number of marks to the score for this row
@@ -25,43 +24,70 @@ const Board = () => {
 
     return (
     <Container className="light-gray-bg">
-      <Row className="main-red p-2">
-        <Col>
-            <div onClick={() => dispatch(toggleBoxValue({rowSpot: 'row1', numberIndex: 0}))} className='main-red-text'>
-                <NumberBox value={row1[0]} display={row1[0]}/>
-            </div>
-        </Col>
-        <Col>
-            <div onClick={() => dispatch(toggleBoxValue({rowSpot: 'row1', numberIndex: 1}))} className='main-red-text'>
-                <NumberBox value={row1[1]} display={row1[1]}/>
-            </div>
-        </Col>
-        <Col>
-            <div onClick={() => dispatch(toggleBoxValue({rowSpot: 'row1', numberIndex: 2}))} className='main-red-text'>
-                <NumberBox value={row1[2]} display={row1[2]}/>
-            </div>
-        </Col>
-        <Col>
-            <div onClick={() => dispatch(toggleBoxValue({rowSpot: 'row1', numberIndex: 3}))} className='main-red-text'>
-                <NumberBox value={row1[3]} display={row1[3]}/>
-            </div>
-        </Col>
-        <Col>
-            <div onClick={() => dispatch(toggleBoxValue({rowSpot: 'row1', numberIndex: 4}))} className='main-red-text'>
-                <NumberBox value={row1[4]} display={row1[4]}/>
-            </div>
-        </Col>
-        <Col>
-            <div onClick={() => dispatch(toggleBoxValue({rowSpot: 'row1', numberIndex: 5}))} className='main-red-text'>
-                <NumberBox value={row1[5]} display={row1[5]} />
-            </div>
-        </Col>
-        <Col>
-            <div onClick={() => dispatch(toggleBoxValue({rowSpot: 'row1', numberIndex: 6}))} className='main-red-text'>
-                <NumberBox value={row1[6]} display="8" />
-            </div>
-        </Col>
-      </Row>
+        <Row className="main-red p-2">
+            {row1.map((current, index) => {
+                    return <Col>
+                        <div onClick = {() => dispatch(toggleBoxValue({rowSpot: 'row1', numberIndex: index}))} className='main-red-text'>
+                            <NumberBox value={current} display={`${index+2}`} />
+                        </div>
+                    </Col>
+                } 
+            )}
+            <Col>
+                <div className='main-red-text' >
+                    <LockBox value={row1[10]} />
+                </div>
+            </Col>
+        </Row>
+
+      <Row className="main-yellow p-2">
+        {row2.map((current, index) => {
+                    return <Col>
+                        <div onClick = {() => dispatch(toggleBoxValue({rowSpot: 'row2', numberIndex: index}))} className='main-yellow-text'>
+                            <NumberBox value={current} display={`${index+2}`} />
+                        </div>
+                    </Col>
+                }
+            )}
+            <Col>
+                <div className='main-yellow-text' >
+                    <LockBox value={row2[10]}/>
+                </div>
+            </Col>
+        </Row>
+
+        <Row className="main-green p-2">
+        {row3.map((current, index) => {
+                    return <Col>
+                        <div onClick = {() => dispatch(toggleBoxValue({rowSpot: 'row3', numberIndex: index}))} className='main-green-text'>
+                            <NumberBox value={current} display={`${index+2}`} />
+                        </div>
+                    </Col>
+                }
+            )}
+            <Col>
+                <div className='main-green-text' >
+                    <LockBox value={row3[10]}/>
+                </div>
+            </Col>
+        </Row>
+
+        <Row className="main-blue p-2">
+            {row4.map((current, index) => {
+                    return <Col>
+                        <div onClick = {() => dispatch(toggleBoxValue({rowSpot: 'row4', numberIndex: index}))} className='main-blue-text'>
+                            <NumberBox value={current} display={`${index+2}`} />
+                        </div>
+                    </Col>
+                    }
+                )
+            }
+            <Col>
+                <div className='main-blue-text' >
+                    <LockBox value={row4[10]}/>
+                </div>
+            </Col>
+        </Row>
 
         <Row>
             <Col>Penalties</Col>
--- src/components/NumberBox.js
+++ src/components/NumberBox.js
@@ -1,6 +1,6 @@
-const NumberBox = ({value}) => {
+const NumberBox = ({value, display}) => {
   if (!value) { //if the box has NOT been checked, it's value in the array will be 0
-    return <div className='number-box-square'>{value}</div>
+    return <div className='number-box-square'>{display}</div>
   } else {  //if the box HAS been checked, the value in the array will be 1
     return <div className='number-box-square'>X</div>
   }
